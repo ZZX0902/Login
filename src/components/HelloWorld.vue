60da7e21f7921506fc4b02417346111f27e3f8ff
zhangzhaoxiang <1448691084@qq.com>
2019-07-15 10:25:07 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <input type="text" v-model="loginForm.username" placeholder="用户名"/>
+    <input type="text" v-model="loginForm.password" placeholder="密码"/>
+    <button @click="login">登录</button>
+  </div></template>
+
+<script>
+
+  export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  // methods:{
+  //   login:function () {
+  //       axios.get("https://jsonplaceholder.typicode.com/posts", {
+  //
+  //       })
+  //         .then(function (data) {
+  //           console.log(data)
+  //         });
+  //
+  //   }
+  //
+
+
+  //
+    methods:{
+    login:function () {
+      this.$axios({
+        method:'post',
+        url:'https://jsonplaceholder.typicode.com/posts',
+        data:this.qs.stringify({
+          userId:this.loginForm.username,
+          title:this.loginForm.password,
+        })
+      }).then((response) =>{
+        console.log(response.data.title)
+      }).catch((error) =>{
+        console.log(error)
+      })
+    }
+    },
+    created(){
+
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
